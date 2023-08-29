aa2c56470212b6cc8215fc16a9b84e3cd7bc704d
branchwelder <htwiggsmith@gmail.com>
2023-08-29 16:55:29 -0700

    Sticky Gutters

--- examples/pixelArt/pixelArt.js
+++ examples/pixelArt/pixelArt.js
@@ -4,6 +4,7 @@ import { BimpEditor } from "/BimpEditor";
 import { brush, flood, line, rect, shift, pan } from "/tools";
 import { toolbox } from "/toolbox";
 import { controlPanel } from "/controlPanel";
+import { numberGutter } from "/numberGutter";
 
 import { drawingCanvas } from "/drawingCanvas";
 import { outline } from "/outline";
@@ -22,9 +23,21 @@ const startPalette = [
   "#ad6dca",
 ];
 
+const needlePalette = ["#20344c", "#faead6"];
+
+function bottomLeft({ bitmap }, gutterPos, size) {
+  if (gutterPos == "bottom" || gutterPos == "top") {
+    return Array.apply(null, Array(bitmap.width)).map((x, i) => i + 1);
+  } else if (gutterPos == "left" || gutterPos == "right") {
+    return Array.apply(null, Array(bitmap.height))
+      .map((x, i) => i + 1)
+      .reverse();
+  }
+}
+
 export function pixelArt({ parent = document.body }) {
   let startState = {
-    bitmap: Bimp.empty(100, 100, 1),
+    bitmap: Bimp.empty(10, 10, 0),
     selection: [],
     aspectRatio: [1, 1],
     scale: 1,
@@ -42,6 +55,18 @@ export function pixelArt({ parent = document.body }) {
         container: "sidebarPrimary",
       }),
       controlPanel(),
+      numberGutter({ size: 20, gutterPos: "left", gutterFunc: bottomLeft }),
+      numberGutter({
+        size: 20,
+        gutterPos: "right",
+        gutterFunc: bottomLeft,
+      }),
+      numberGutter({ size: 20, gutterPos: "top", gutterFunc: bottomLeft }),
+      numberGutter({
+        size: 20,
+        gutterPos: "bottom",
+        gutterFunc: bottomLeft,
+      }),
     ],
   });
 
--- numberGutter.js
+++ numberGutter.js
@@ -1,67 +1,150 @@
 import { html, render } from "lit-html";
 
-export function numberGutter({ axis, size }) {
-  return ({ state, parent }) => {
-    const arr = Array.apply(
-      null,
-      Array(axis == "horizontal" ? state.bitmap.width : state.bitmap.height)
-    ).map((x, i) => i);
+function numberArr({ bitmap }, gutterPos, size) {
+  return Array.apply(
+    null,
+    Array(
+      gutterPos == "bottom" || gutterPos == "top" ? bitmap.width : bitmap.height
+    )
+  ).map((x, i) => i + 1);
+}
 
-    function checkHighlight(num) {
-      if (axis == "horizontal" && state.pos.x == num) return true;
-      if (axis == "vertical" && state.pos.y == num) return true;
+export function numberGutterExtension(
+  { state, parent },
+  { gutterPos, size, container = "workspace", gutterFunc = numberArr }
+) {
+  let { bitmap, pan, pos, aspectRatio, scale } = state;
+  let arr = gutterFunc(state, gutterPos, size);
+
+  const dom = document.createElement("div");
+  parent[container].appendChild(dom);
+
+  function checkHighlight(num) {
+    if ((gutterPos == "bottom" || gutterPos == "top") && pos.x == num)
+      return true;
+    if ((gutterPos == "left" || gutterPos == "right") && pos.y == num)
+      return true;
+  }
+
+  function updatePosition() {
+    const bbox = parent[container].getBoundingClientRect();
+
+    let offX, offY;
+
+    if (gutterPos == "left") {
+      offX = pan.x - size;
+      offX = offX < 0 ? 0 : offX;
+      offY = pan.y;
+    } else if (gutterPos == "right") {
+      offX = pan.x + bitmap.width * aspectRatio[0] * scale;
+      offX = offX + size > bbox.width ? bbox.width - size : offX;
+      offY = pan.y;
+    } else if (gutterPos == "top") {
+      offY = pan.y - size;
+      offY = offY < 0 ? 0 : offY;
+      offX = pan.x;
+    } else if (gutterPos == "bottom") {
+      offY = pan.y + bitmap.height * aspectRatio[1] * scale;
+      offY = offY + size > bbox.height ? bbox.height - size : offY;
+      offX = pan.x;
     }
 
-    const template = html`<style>
-        .vertical {
-          display: flex;
-          flex-direction: column;
-          transform: translateY(${state.pan.y}px);
-          width: ${size};
-          min-height: 0;
-          min-width: 0;
-        }
+    dom.style.transform = `translate(${offX}px, ${offY}px)`;
+  }
 
-        .vertical .cell {
-          flex: 0 0 ${state.aspectRatio[1] * state.scale}px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          min-height: 0;
-          min-width: 0;
-        }
+  function updateSize() {
+    if (gutterPos == "bottom" || gutterPos == "top") {
+      dom.style.width = `${bitmap.width * aspectRatio[0] * scale}px`;
+    } else if (gutterPos == "left" || gutterPos == "right") {
+      dom.style.height = `${bitmap.height * aspectRatio[1] * scale}px`;
+    }
+  }
 
-        .horizontal {
-          display: flex;
-          transform: translateX(${state.pan.x}px);
-          height: ${size};
-          min-height: 0;
-          min-width: 0;
-        }
+  function view() {
+    render(
+      html`<style>
+          .gutter {
+            background-color: #2d2c2c;
+            font-family: monospace;
+            font-weight: bold;
+            color: #969696;
+            user-select: none;
+            border: 1px solid black;
+          }
+          .left,
+          .right {
+            display: flex;
+            flex-direction: column;
+            width: ${size}px;
+          }
 
-        .horizontal .cell {
-          flex: 0 0 ${state.aspectRatio[0] * state.scale}px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          min-height: 0;
-          min-width: 0;
-        }
+          .left .cell,
+          .right .cell {
+            flex: 0 0 ${aspectRatio[1] * scale}px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
 
-        .highlight {
-          background-color: #00000044;
-        }
-      </style>
-      <div class=${axis}>
-        ${arr.map(
-          (num) =>
-            html`<div class="cell ${checkHighlight(num) ? "highlight" : ""}">
-              ${num + 1}
-            </div>`
-        )}
-      </div>`;
+          .top,
+          .bottom {
+            display: flex;
+            height: ${size}px;
+          }
 
-    render(template, parent);
+          .top .cell,
+          .bottom .cell {
+            flex: 0 0 ${aspectRatio[0] * scale}px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .gutter > .cell:nth-of-type(odd) {
+            background: #00000022;
+          }
+
+          .highlight {
+            background-color: #00000044 !important;
+            color: #fafafa !important;
+          }
+        </style>
+        <div class="gutter ${gutterPos}">
+          ${arr.map(
+            (content, index) =>
+              html`<div
+                class="cell ${checkHighlight(index) ? "highlight" : ""}">
+                ${content}
+              </div>`
+          )}
+        </div>`,
+      dom
+    );
+  }
+
+  return {
+    syncState(state) {
+      ({ bitmap, pan, pos, aspectRatio, scale } = state);
+      arr = gutterFunc(state, gutterPos, size);
+
+      updatePosition();
+      updateSize();
+      view(state);
+
+      // if (
+      //   bitmap.width != this.width ||
+      //   bitmap.height != this.height ||
+      //   pan != this.pan ||
+      //   pos != this.pos
+      // ) {
+      //   this.width = bitmap.width;
+      //   this.height = bitmap.height;
+      //   this.pan = pan;
+      // }
+    },
   };
+}
+
+export function numberGutter(options = {}) {
+  return (config) => numberGutterExtension(config, options);
 }
